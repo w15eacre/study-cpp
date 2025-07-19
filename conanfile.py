c54f0aa71e92828da308d27ed2b8801284ea9cf1
w15dev <oleg.kuznetsov@internet.ru>
2025-07-19 20:38:16 +0300

    Add ccache tool requirement to conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class StudyCppConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
     requires = ["gtest/1.16.0"]
-    tool_requires = ["cmake/3.30.0", "ninja/1.11.1"]
+    tool_requires = ["cmake/3.30.0", "ninja/1.11.1", "ccache/4.11"]
 
     def generate(self):
         tc = CMakeToolchain(self)
